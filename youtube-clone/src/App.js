67ca5e7c5acb8fe7dd4d24c5be67530909c85cf5
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2022-09-12 20:11:51 -0600

    Create Menu.jsx
    Navigation menu has been created which will allow user to navigate to different sections of the application

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import Menu from "./Components/Menu";
+import Navbar from "./Components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div`
+  flex: 7;
+`;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper></Wrapper>
+        Main Body
+      </Main>
+    </Container>
   );
 }
 
