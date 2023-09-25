51b807be1dad7e8bef8eb0409638e00758aa749e
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-09-25 00:58:55 -0600

    Update Homepage.jsx

--- youtube-clone/src/Pages/Homepage.jsx
+++ youtube-clone/src/Pages/Homepage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Card from "../Components/Card";
 
-function Homepage() {
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
+  );
+};
 
-export default Homepage
+export default Home;
