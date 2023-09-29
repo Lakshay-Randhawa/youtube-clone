38b654f6b470eeb85cc83f686a1ce1d443392365
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2023-09-28 23:02:19 -0600

    Create useFetchVideos.js

---
+++ youtube-clone/src/Hooks/useFetchVideos.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const options = {
+  method: "GET",
+  url: "https://youtube138.p.rapidapi.com/home/",
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const useFetchVideos = () => {
+  const [videos, setVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.request(options);
+        setVideos(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchVideos();
+  }, []);
+
+  return { videos, isLoading, error };
+};
